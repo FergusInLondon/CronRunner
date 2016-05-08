2a571bc686f0d1608f14b7db92ffbc5b4d6d6be9
Fergus Morrow <fergus@fergus-morrow.com>
2016-05-08 23:30:53 +0100

    Hack up a --debug flag for CLI.

--- Runner.php
+++ Runner.php
@@ -13,12 +13,80 @@ class Runner {
     private $container;
 
     public function __construct(
-        \Pimple\Container $container
+        \Pimple\Container $container,
+        $debug = false
     ) {
         $this->container = $container;
+        
+        if ($debug){
+            return $this->runAllJobs();
+        }
+        
         $this->configureJobs();
     }
+
+
+    /**
+     *
+     *
+     */
+    private function listJobDirectoryEntries() {
+        $jobsFolder    = $this->container['Config']->getItem('jobs_folder', '/Jobs');
+        
+        // Retrieve list of files in the /Jobs directory
+        return array_diff(
+            scandir(__DIR__.$jobsFolder),
+            array('..', '.', 'Base')
+        );
+    }
     
+    
+    /**
+     *
+     *
+     */
+    private function getJobObjectForFile($file) {
+        $jobsNamespace = $this->container['Config']->getItem('jobs_namespace', '\CronRunner\Jobs\\');
+        $class = $jobsNamespace . substr($fileName, 0, -4);
+        
+        // Check it's a PHP file and extends Base/Job.
+        if (strpost(fileName, '.php') && is_subclass_of($class, '\CronRunner\Jobs\Base\Job')) {
+            return new $class( $this->container );
+        }
+        
+        return false;
+    }
+    
+
+    /**
+     *
+     *
+     */
+    private function runAllJobs() {
+        
+        $jobFiles = $this->listJobDirectoryEntries();
+        
+        foreach ($jobFiles as $jobFilename) {
+            printf("\nFound new Job: '%s'\n", $jobFilename);
+            
+            $job = $this->getJobObjectForFile($jobFilename);
+            
+            if (!$job) {
+                printf("\t[%s] Job not valid!\n", $jobFilename);
+                continue;
+            }
+
+            printf("\t[%s] Valid Job. Executing...\n", $jobFilename);
+
+            $jobObject = new $job;
+            $return    = $job->execute();
+
+            printf("\t[%s] Executed - returning '%s'.\n", $jobFilename, $return);
+        }
+        
+        printf("\nFinished running jobs.\n");
+    }
+
 
     /**
      * Dynamically load all available Job objects, creating instances and 
@@ -26,29 +94,19 @@ class Runner {
      */
     private function configureJobs() {
         $scheduler     = $this->container['Scheduler'];
-        $jobsFolder    = $this->container['Config']->getItem('jobs_folder', '/Jobs');
         $jobsNamespace = $this->container['Config']->getItem('jobs_namespace', '\CronRunner\Jobs\\');
 
-        // Retrieve list of files in the /Jobs directory
-        $classFiles = array_diff(
-            scandir(__DIR__.$jobsFolder),
-            array('..', '.', 'Base')
-        );
+        $classFiles = $this->listJobDirectoryEntries();
         
         // Iterate through all files found, verify that they are valid Job 
         //  classes, and then instantiate and configure.
         foreach ($classFiles as $fileName) {
-            if (!strpos($fileName, '.php')) {
-                continue;
-            }
-            
-            $class = $jobsNamespace . substr($fileName, 0, -4);
-            
-            if (is_subclass_of($class, '\CronRunner\Jobs\Base\Job')) {
-                $job = new $class( $this->container );
+            $job = $this->getJobObjectForFile($fileName);
+
+            if($job){
                 $job->configure(
                     $this->container['Scheduler']->call([$job, 'execute'])
-                );
+                );            
             }
         }
     }
