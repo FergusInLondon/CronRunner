3fdf419f85bca2df122be64ef37a7b3a4581b298
Fergus Morrow <fergus@fergus-morrow.com>
2016-05-08 23:03:32 +0100

    Update examples for new usage.

--- Jobs/ExampleJob.php
+++ Jobs/ExampleJob.php
@@ -1,19 +1,16 @@
 <?php namespace CronRunner\Jobs;
 
 class ExampleJob extends Base\Job {
-    
-    public function configure(\GO\Scheduler $scheduler) {
+    public function configure(\GO\Job\Job $job) {
         $logOutput  = $this->configuration->getItem('output_file', 'logs/output.log');
         $adminEmail = $this->configuration->getItem('output_email', 'admin@localhost');
-
-        $scheduler->call([$this, 'execute'])
-            ->every()->minute()//->hour(06)
-            ->output( sprintf("%s/../%s", __DIR__, $logOutput), true)
+        
+        $job->every()->hour(06)
+            ->output(sprintf("%s/../%s", __DIR__, $logOutput), true)
             ->email($adminEmail);
     }
 
     public function execute() {
        return "We're running without any intervention required.";
     }
-
 }
--- Jobs/LoggedExampleJob.php
+++ Jobs/LoggedExampleJob.php
@@ -1,8 +1,7 @@
 <?php namespace CronRunner\Jobs;
 
 class LoggedExampleJob extends Base\Job {
-    
-    public function configure(\GO\Scheduler $scheduler) {
+    public function configure(\GO\Job\Job $job) {
         $logOutput  = $this->configuration->getItem('monolog_file', 'logs/test.log');
 
         $logger = new \Monolog\Logger('logOutput');
@@ -10,8 +9,7 @@ class LoggedExampleJob extends Base\Job {
             new \Monolog\Handler\StreamHandler(sprintf("%s/../%s", __DIR__, $logOutput), \Monolog\Logger::INFO)
         );
 
-        $scheduler->call([$this, 'execute'])
-            ->every()->minute()
+        $job->every()->minute()
             ->setLogger($logger)
             ->setLabel('LoggedExampleJob')
             ->setJobDoneMessage('Job Completed.');
@@ -20,5 +18,4 @@ class LoggedExampleJob extends Base\Job {
     public function execute() {
        return "We're now logging via MonoLog.\n";
     }
-
 }
